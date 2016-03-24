16465d4922a25e53bdc42c6de9305ea9d767f51b
IgorXsky <igorxsky@gmail.com>
2016-03-24 02:31:47 +0200

    My calculator

--- 16.03.2016/index.php
+++ 16.03.2016/index.php
@@ -1,10 +1,10 @@
 <?php
 
-define('MAX_SUM', '500');
-define('MIN_SUM', '100000');
+define('MAX_SUM', '1000000'); //максимальная сумма кредита
+define('MIN_SUM', '100'); //минимальная сумма кредита
 define('MAX_PAY', '99');
 define('MIN_PAY', '1');
-
+define('MONTH', '12');
 
 $errorMessage = '';
 $errors = [];
@@ -12,23 +12,39 @@ $errors = [];
 $sum = '';
 $payment = '';
 $term = '';
+$persent = '';
 
 if(!empty($_GET)) {
-    $term = $_GET['term'];
-    if($_GET['sum'] <= MAX_SUM || $_GET['sum'] >= MIN_SUM ){
+    switch ($_GET['term']) {
+        case 1: $term = 1; $persent = 0.09; break;
+        case 2: $term = 2; $persent = 0.11; break;
+        case 3: $term = 3; $persent = 0.13; break;
+        case 4: $term = 4; $persent = 0.15; break;
+        case 5: $term = 5; $persent = 0.17; break;
+        case 6: $term = 6; $persent = 0.19; break;
+        case 7: $term = 7; $persent = 0.09; break;
+        default: $errors[] = 'error in term';
+    }
+    if($_GET['sum'] <= MAX_SUM && $_GET['sum'] >= MIN_SUM ){
         $sum = $_GET['sum'];
     }else{
         $errors[] = "Вы ввели неверную суму";
     }
-    if($_GET['payment'] <= MAX_PAY || $_GET['payment'] >= MIN_PAY){
-        $payment = $_GET['payment'];
+    if($_GET['payment'] <= MAX_PAY && $_GET['payment'] >= MIN_PAY){
+        $payment = $_GET['payment']/100;
     }else{
-        $errors[] = "Вы ввели неверный первоначальный взнос d %";
+        $errors[] = "Вы ввели неверный первоначальный взнос в %";
     }
 }
 
+$result = ($sum - ($sum*$payment)) + (($sum*$persent)*$term);
+
+$month = $term*MONTH;
+
+$payToMonth = $result/$month;
+
 $errorMessage = implode("<br>", $errors);
 
-echo $errorMessage;
+
 
 include "form.phtml";
