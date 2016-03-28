4d1ac44f9d499106d2780625272776d9f3f706fc
IgorXsky <igorxsky@gmail.com>
2016-03-28 16:32:59 +0300

    Task 1

--- arrays_loops_tasks/1.php
+++ arrays_loops_tasks/1.php
@@ -1,5 +1,7 @@
 <?php
 
+//Дан массив с элементами 'html', 'css', 'php', 'js', 'jq'. С помощью цикла foreach выведите эти слова в столбик
+
 $arr = ['html', 'css', 'php', 'js', 'jq'];
 
 foreach($arr as $item){
