1e6ee834295f773cd8f73274ef7cc9c8fe7b6609
IgorXsky <igorxsky@gmail.com>
2016-04-08 17:03:21 +0300

    Task2 I think

--- 02.04.2016/Task2.php
+++ 02.04.2016/Task2.php
@@ -1,8 +1,26 @@
 <?php
 /* Написать функцию insert($connection, $table, $data).
-Функция должна добавлять в таблицу $table новую запись с данными из массива $data где ключ массива это поле (колонка),
+Функция должна добавлять в таблицу $table новую запись с данными из
+массива $data где ключ массива это поле (колонка),
 а значение массива это сама запись. */
 
-function insert($connection, $table, $data){
-    
+$connection = mysqli_connect('localhost', 'root', '') or die('Could not connect: ' . mysqli_error($connection));
+mysqli_select_db($connection, 'homework') or die('Could not select database');
+$data = ['id' => '1',
+        'name' => 'Igor',
+        'surname' => 'Pavliuk',
+        ];
+
+function insert($connection, $table, $data)
+{
+    $keys = "'id', 'name', 'value'";
+    $values = "'{$data['id']}', '{$data['name']}', '{$data['surname']}'";
+    $query = "INSERT INTO {$table}  ( {$keys} )
+              VALUES ( {$values} )";
+    $result = mysqli_query($connection, $query) or die('Query failed: ' . mysqli_error($connection));
+    return $result;
 }
+
+insert($connection, 'students', $data);
+print_r($result);
+
