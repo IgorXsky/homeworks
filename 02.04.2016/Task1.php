16cfc25f093dbfa74fd321846a07ae6296c1d9ce
IgorXsky <igorxsky@gmail.com>
2016-04-08 08:26:04 +0300

    Task 1

---
+++ 02.04.2016/Task1.php
@@ -0,0 +1,13 @@
+<?php
+/* Написать функцию select($connection, $table, $id = null).
+Функция должна делать SELECT запрос в таблицу $table.
+Если передан необязательный параметр $id то функция должна выбирать одну запись с указанным id. */
+
+
+function select($connection, $table, $id = null){
+    if(isset($id)){
+        $query = 'SELECT * FROM' . "$table" . 'WHERE id =' . "$id";
+    }else{
+        $query = 'SELECT * FROM' . "$table";
+    }
+}
